f20d2e133c7c980f7c4e1671c8cc46d38d0d8486
NearDrive <NearDrive@gmail.com>
2026-02-26 16:44:10 +0100

    PR81: restore replay baseline prefixes to 8f0b

--- tests/Game.Server.Tests/BaselineChecksums.cs
+++ tests/Game.Server.Tests/BaselineChecksums.cs
@@ -3,5 +3,5 @@ namespace Game.Server.Tests;
 internal static class BaselineChecksums
 {
     public const string ScenarioBaselinePrefix = "efaa";
-    public const string ReplayBaselinePrefix = "298a";
+    public const string ReplayBaselinePrefix = "8f0b";
 }
